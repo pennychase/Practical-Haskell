e5e71a6f20674edbb36e698efbad05351d2d495b
Penny Chase <mpchase@mac.com>
2020-09-01 19:44:00 -0400

    Chapter 5 Profiling

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,8 @@
 module Main where
 
-
+-- This main function will be used to experiment wth profiling
 main :: IO ()
-main = undefined
+main = putStrLn $ show result
+
+result :: Integer
+result = foldr (*) 1 [1 .. 100000]
