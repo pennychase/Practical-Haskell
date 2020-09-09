4d95b2a119186347c831f9c3b28ef6b6ce9292e0
Penny Chase <mpchase136@gmail.com>
2020-09-08 20:44:24 -0400

    CHapter 7 FInished Exercise 1

--- src/Chapter7/Exercises.hs
+++ src/Chapter7/Exercises.hs
@@ -2,10 +2,12 @@ module Chapter7.Exercises where
 
 import Data.List
 
+brokenThreeJumps :: Int -> [Int] -> [Int] -> [Int] -> [Int]
 brokenThreeJumps y as bs cs =
     let x = brokenThreeJumps' as bs cs
     in map ((y +) . head) $ group $ sort x
 
+brokenThreeJumps' :: [Int] -> [Int] -> [Int] -> [Int]
 brokenThreeJumps' as bs cs = do
     a <- as
     b <- bs
@@ -14,18 +16,19 @@ brokenThreeJumps' as bs cs = do
 
 brokenJumps :: Int -> Int -> [Int] -> [Int]
 brokenJumps year n jumps =
-    let x = brokenJumps' n jumps jumps
+    let x = brokenJumps' (replicate (n) jumps) (replicate (length jumps) 0)
     in map ((year +) . head) $ group $ sort x
 
-brokenJumps' :: Int -> [Int] -> [Int] -> [Int]
-brokenJumps' n jumps combinations = do
-    if n == 0
-    then return $ combinations 
-    else do
-        j <- jumps
-        c <- combinations
-        r <- j + c
-        return $ brokenJumps' (n-1) (j+c) 
- 
+brokenJumps' :: [[Int]] -> [Int] -> [Int]
+brokenJumps' [] acc = acc
+brokenJumps' (xs:xss) acc =
+    let newAcc = brokenJumps' xss acc
+    in [ x + y | x <- xs, y <- newAcc ]
 
-
+brokenJumps'' :: [[Int]] -> [Int] -> [Int]
+brokenJumps'' [] acc = return acc
+brokenJumps'' (xs:xss) acc = do
+    newAcc <- brokenJumps'' xss acc
+    y <- newAcc
+    x <- xs
+    return $! x + y
