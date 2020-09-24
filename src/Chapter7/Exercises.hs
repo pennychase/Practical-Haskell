ab9c3fa016ae4f1fd6b2c13b2330f8a0172d02f6
Penny Chase <mpchase@mac.com>
2020-09-23 20:30:41 -0400

    Chapter 7 Exercise 7.6 (factorial with two states)

--- src/Chapter7/Exercises.hs
+++ src/Chapter7/Exercises.hs
@@ -2,6 +2,9 @@ module Chapter7.Exercises where
 
 import Data.List
 import Control.Monad
+import Control.Monad.Reader
+import Control.Monad.Writer
+import Control.Monad.State
 
 -- Exercise 7.1
 
@@ -48,5 +51,65 @@ brokenJumps2 year n jumps =
 
 -- Exercise 7.2
 find_  :: (a -> Bool) -> [a] -> Maybe a
-find_ f xs = msum $ (\x -> case f x of True -> Just x; False -> Nothing) <$> xs
+find_ f xs = msum $ map (\x -> case f x of True -> Just x; False -> Nothing) xs
                     
+-- Monads
+
+addPrefix :: String -> Reader String String
+addPrefix s = ask >>= \p -> return $ p ++ s
+
+-- runReader (addPrefix "hello") "++"  => "++hello"
+-- runReader (mapM addPrefix ["hello", "world"]) "++" => ["++hello", "++world"]
+
+logInformation :: [String] -> Writer String ()
+logInformation = mapM_ (\s -> tell (s ++ "\n"))
+
+-- runWriter $ logInformation ["hello", "world"] => ((), "hello\nworld\n")
+
+mySequence :: (Monad m) => [ m a] -> m [a]
+mySequence [] = return []
+mySequence (x:xs) = do
+    y <- x
+    ys <- mySequence xs
+    return (y:ys)
+
+mySequence' :: (Monad m) => [m a] -> m [a]
+mySequence' [] = return []
+mySequence' (x:xs) = x                  >>= \y ->
+                     mySequence' xs     >>= \ys ->
+                     return (y:ys)
+
+myMapM :: (Monad m) => (a -> m b) -> [a] -> m [b]
+myMapM f [] = return []
+myMapM f (x:xs) = do
+    y <- f x
+    ys <- myMapM f xs
+    return (y:ys)
+
+myMapM' :: (Monad m) => (a -> m b) -> [a] -> m [b]
+myMapM' f = sequence . map f
+
+myFilterM :: (Monad m) => (a -> m Bool) -> [a] -> m [a]
+myFilterM f [] = return []
+myFilterM f (x:xs) = do
+    y <- f x
+    ys <- myFilterM f xs
+    if y == True
+        then return (x:ys)
+        else return ys
+
+factorialSteps :: Integer -> Writer (Sum Integer) Integer
+factorialSteps n = foldM (\f x -> tell (Sum 1) >> return (f*x)) 1 [1 .. n]
+
+-- The inner State is the accumulator and the outer StateT is the counter
+factorialState' :: StateT Integer (State Integer) Integer
+factorialState' = do
+    step <- get
+    res <- lift $ get
+    if step == 0 then return res else do
+        lift $ put (res * step)
+        put (step - 1)
+        factorialState'
+
+factorialState :: Integer -> Integer
+factorialState n = execState (execStateT factorialState' n) 1
